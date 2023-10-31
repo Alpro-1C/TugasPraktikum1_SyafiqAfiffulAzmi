7baa373f0cd0b4650c7945b7774d64ed47bb62a1
Boruman <147920803+Boruman@users.noreply.github.com>
2023-10-31 09:12:10 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A,B,C;
+
+    cout<< "Masukan Angka Pertama : ";
+    cin>> A;
+    cout<< "Masukan Angka Kedua : ";
+    cin>> B;
+    cout<< "Masukan Angka Ketiga : ";
+    cin>> C;
+
+    //memasukan angka untuk di tampilkan mana yanag terbesar
+
+    int besar = A;
+    //A menjadi yang terbesar
+    if (B>besar){
+    //B menjadi yang terbesar jika lebih besar dari A
+    besar=B;
+    }if (C>besar){
+    //C menjadi yang terbesar jika lebih besar dari B
+    besar=C;
+    }
+    cout << "Angka Terbesar : " << besar<<endl;
+    //output menunjukan angka yang terbesar
+
+    return 0;
+}
